664ed089a980b30a605b46ff00fef59740b2e771
24A31A4660 <akshaykountesh001@gmail.com>
2026-07-04 01:09:20 +0530

    Decrease translateX to -80px in Session 4

--- js/earth.js
+++ js/earth.js
@@ -520,7 +520,7 @@
                 if (idx === 0) {
                     canvasEl.style.transform = 'translateX(180px)';
                 } else if (idx === 3) {
-                    canvasEl.style.transform = 'translateX(-60px)';
+                    canvasEl.style.transform = 'translateX(-80px)';
                 } else {
                     canvasEl.style.transform = 'translateX(0px)';
                 }
